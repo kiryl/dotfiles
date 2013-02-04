2e7e38ff00840136f97ccbe4d0f461c19bd7c176
Kirill A. Shutemov <kirill@shutemov.name>
2013-02-04 13:44:05 +0200

    awesome: bind xlock to modkey+F12
    Signed-off-by: Kirill A. Shutemov <kirill@shutemov.name>

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -233,6 +233,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
+    awful.key({ modkey,           }, "F12", function () awful.util.spawn("xlock") end),
 
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
